06a81fcfb1a2ae601b76bb437e7b53665946cf7b
Tejasvi raj <tejasviraj8686@gmail.com>
2024-12-08 23:41:10 +0530

    Enhance Header component: Add user details retrieval and display, including profile picture and welcome message. Implement search input with styling for improved user experience.

--- app/Components/Header.jsx
+++ app/Components/Header.jsx
@@ -1,14 +1,57 @@
-import { View, Text } from "react-native";
-import React, { useEffect } from "react";
+import { View, Text, Image, TextInput, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
 import { client } from "../Utils/KindConfig";
+import Color from "../Utils/Color";
 
 export default function Header() {
+  const [userDetail, setUserDetail] = useState();
   useEffect(() => {
     getUserDetails();
   }, []);
   const getUserDetails = async () => {
     const user = await client.getUserDetails();
+    setUserDetail(user);
     console.log(user);
   };
-  return <View style={{ padding: 20, marginTop: 25 }}></View>;
+  return (
+    <>
+      <View
+        style={{
+          padding: 10,
+          marginTop: 15,
+          display: "flex",
+          flexDirection: "row",
+          gap: 5,
+          alignItems: "center",
+        }}
+      >
+        <Image
+          source={{ uri: userDetail?.picture }}
+          style={{ width: 45, height: 45, borderRadius: 99 }}
+        />
+
+        <View>
+          <Text style={{ fontSize: 18, fontFamily: "outfit" }}>Welcome</Text>
+          <Text
+            style={{ fontSize: 20, fontWeight: "bold", color: Color.PRIMARY }}
+          >
+            Hello {userDetail?.given_name}
+          </Text>
+        </View>
+      </View>
+      <View style={styles.input}>
+        <TextInput placeholder="Search" />
+      </View>
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  input: {
+    backgroundColor: Color.WHITE,
+    borderRadius: 99,
+    padding: 10,
+    paddingHorizontal: 20,
+    marginTop: 20,
+  },
+});
