44e5cf41d01d77f4ccdff5915d1f9722069c707f
Tejasvi raj <tejasviraj8686@gmail.com>
2024-12-08 14:13:55 +0530

    Enhance authentication flow in index.tsx: Implement user authentication check on component mount and add basic styling for the main view

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,33 @@
-import { Text, View } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { Text, View, StyleSheet } from "react-native";
 import LoginScreen from "./Screens/LoginScreen";
+import { useEffect } from "react";
+import { client } from "./Utils/KindConfig";
 
 export default function Page() {
+  useEffect(() => {
+    checkAuthenticate();
+  }, []);
+  const checkAuthenticate = async () => {
+    // Using `isAuthenticated` to check if the user is authenticated or not
+    if (await client.isAuthenticated) {
+      // Need to implement, e.g: call an api, etc...
+    } else {
+      // Need to implement, e.g: redirect user to sign in, etc..
+    }
+  };
+
   return (
-    <View >
-        <LoginScreen />
-       
+    <View style={styles.container}>
+      <LoginScreen />
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
